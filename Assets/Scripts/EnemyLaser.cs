5e9038bfa02f570fbd6d06792377f65e91d72ef3
wiggansj <wiggansj@live.com>
2020-06-23 16:55:15 +0200

    update EnemyLaser.cs

--- Assets/Scripts/EnemyLaser.cs
+++ Assets/Scripts/EnemyLaser.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class EnemyLaser : MonoBehaviour
 {
     [SerializeField]
     private float _enemylaserSpeed = 10.0f;
+    private bool _up = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,8 @@ public class EnemyLaser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      if (_up == false)
+      {
         transform.Translate(Vector3.down * _enemylaserSpeed * Time.deltaTime);
         if (transform.position.y < -22.0f)
         {
@@ -21,5 +24,22 @@ public class EnemyLaser : MonoBehaviour
             }
             Destroy(this.gameObject);
         }
+      } 
+      else
+      {
+         transform.Translate(Vector3.up * _enemylaserSpeed * Time.deltaTime);
+        if (transform.position.y < 22.0f)
+        {
+            if (this.transform.parent != null)
+            {
+                Destroy(this.transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+      }
+    }
+    public void FireUp()
+    {
+      _up = true;
     }
 }
